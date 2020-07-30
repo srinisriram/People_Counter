8bfebcf73144aaf3da57130175578844f4cc4783
Abhisar Anand <abhisar.muz@gmail.com>
2020-07-30 19:25:49 -0400

    Updated the occupation2.py and tried to make it better.

--- people-counting-opencv/occupation2.py
+++ people-counting-opencv/occupation2.py
@@ -70,8 +70,8 @@ def thread_for_capturing_face():
         frame = imutils.resize(frame, width=500)
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        (H, W) = frame.shape[:2]
-
+        if W is None or H is None:
+            (H, W) = frame.shape[:2]
 
         status = "Waiting"
         rects = []
@@ -80,81 +80,84 @@ def thread_for_capturing_face():
             status = "Detecting"
             trackers = []
 
-        blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
-        net.setInput(blob)
-        detections = net.forward()
+            blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+            net.setInput(blob)
+            detections = net.forward()
 
-        for i in np.arange(0, detections.shape[2]):
+            for i in np.arange(0, detections.shape[2]):
 
-            confidence = detections[0, 0, i, 2]
+                confidence = detections[0, 0, i, 2]
 
-            if confidence > 0.4:
+                if confidence > 0.4:
 
-                idx = int(detections[0, 0, i, 1])
+                    idx = int(detections[0, 0, i, 1])
 
-                if CLASSES[idx] != "person":
-                    continue
+                    if CLASSES[idx] != "person":
+                        continue
 
-                box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-                (startX, startY, endX, endY) = box.astype("int")
+                    box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+                    (startX, startY, endX, endY) = box.astype("int")
 
-                tracker = dlib.correlation_tracker()
-                rect = dlib.rectangle(startX, startY, endX, endY)
-                tracker.start_track(rgb, rect)
+                    tracker = dlib.correlation_tracker()
+                    rect = dlib.rectangle(startX, startY, endX, endY)
+                    tracker.start_track(rgb, rect)
 
-                trackers.append(tracker)
+                    trackers.append(tracker)
 
+        else:
+            # loop over the trackers
+            for tracker in trackers:
+                status = "Tracking"
+
+                tracker.update(rgb)
+                pos = tracker.get_position()
+
+                startX = int(pos.left())
+                startY = int(pos.top())
+                endX = int(pos.right())
+                endY = int(pos.bottom())
+
+                rects.append((startX, startY, endX, endY))
+
+        cv2.line(frame, (0, W // 2), (H, W // 2), (0, 255, 255), 2)
+
+        objects = ct.update(rects)
+
+        for (objectID, centroid) in objects.items():
+
+            to = trackableObjects.get(objectID, None)
+
+            if to is None:
+                to = TrackableObject(objectID, centroid)
 
             else:
 
-                for tracker in trackers:
-                    status = "Tracking"
-
-                    tracker.update(rgb)
-                    pos = tracker.get_position()
-
-                    startX = int(pos.left())
-                    startY = int(pos.top())
-                    endX = int(pos.right())
-                    endY = int(pos.bottom())
-
-                    rects.append((startX, startY, endX, endY))
-
-            objects = ct.update(rects)
-
-            for (objectID, centroid) in objects.items():
-
-                to = trackableObjects.get(objectID, None)
-
-                if to is None:
-                    to = TrackableObject(objectID, centroid)
-
-
-                else:
-
-                    y = [c[1] for c in to.centroids]
-                    direction = centroid[1] - np.mean(y)
-                    to.centroids.append(centroid)
+                y = [c[1] for c in to.centroids]
+                direction = centroid[1] - np.mean(y)
+                to.centroids.append(centroid)
 
                 if not to.counted:
 
-                    if direction < 0 and centroid[1] < H // 2:
+                    if direction < 0 and centroid[1] < W // 2:
                         totalUp += 1
                         totalPeople += 1
+                        print(type(totalPeople))
                         to.counted = True
 
-
-                    elif direction > 0 and centroid[1] > H // 2:
-                        totalPeople += 1
+                    elif direction > 0 and centroid[1] > W // 2:
+                        totalPeople -= 1
                         totalDown += 1
                         to.counted = True
 
-        trackableObjects[objectID] = to
+            if totalPeople == 5:
+                print('Sorry you cannot enter.')
+            trackableObjects[objectID] = to
 
-        text = "ID {}".format(objectID)
-        cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+            text = "ID {}".format(objectID)
+            cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+
 
         info = [
             ("Up", totalUp),
@@ -166,29 +169,22 @@ def thread_for_capturing_face():
             text = "{}: {}".format(k, v)
             cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-        total_faces_detected_locally = totalPeople
-
-        if writer is not None:
-            writer.write(frame)
 
         print("{} people entered in today".format(totalPeople))
-
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
 
         if key == ord("q"):
             break
 
-    totalFrames += 1
-    fps.update()
+        totalFrames += 1
+        fps.update()
 
-    fps.stop()
-    print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+        fps.stop()
+        print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+        print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 
-
-    vs.stop()
-    cv2.destroyAllWindows()
+        cv2.destroyAllWindows()
 
 
 def thread_for_zmq_for_receiving_face_detected_by_peer():
