89d3418b0c496fd239a711f2dfee1156b17c75dd
srinisriram <54331317+srinisriram@users.noreply.github.com>
2020-07-31 09:55:15 -0400

    Update occupationEntrance.py

--- people-counting-opencv/occupationEntrance.py
+++ people-counting-opencv/occupationEntrance.py
@@ -195,7 +195,7 @@ def thread_for_zmq_for_receiving_face_detected_by_peer():
     while run_program:
         print("[INFO] Waiting to receive info...")
         #  Wait for next request from client
-        message = socket.recv(1024).decode()
+        message = socket.recv_string()
         print("Received request: %s" % str(message))
         total_faces_detected_by_peer = int(message)
         if total_faces_detected_by_peer != total_faces_detected_locally:
@@ -215,7 +215,7 @@ def thread_for_zmq_for_transmitting_face_detected_locally():
         if total_faces_detected_locally >= curr_count:
             print("[INFO] Sending Info...")
             #  Send the count
-            socket.send((str(total_faces_detected_locally)).encode())
+            socket.send_string(str(total_faces_detected_locally))
             curr_count = total_faces_detected_locally
 
 
