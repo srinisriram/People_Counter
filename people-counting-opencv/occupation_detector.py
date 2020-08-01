a79131633a4a2ea6138e9ad7a778d4754cea3a6e
srinisriram <54331317+srinisriram@users.noreply.github.com>
2020-07-31 23:28:38 -0400

    Update occupation_detector.py

--- people-counting-opencv/occupation_detector.py
+++ people-counting-opencv/occupation_detector.py
@@ -2,9 +2,7 @@ import time
 import threading
 import cv2
 import dlib
-import imutils
 import numpy as np
-from imutils.video import VideoStream, FPS
 from pyimagesearch.centroidtracker import CentroidTracker
 from pyimagesearch.trackableobject import TrackableObject
 import socket
@@ -28,11 +26,8 @@ CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "sofa", "train", "tvmonitor"]
 
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
+vs = cv2.VideoCapture(0)
 time.sleep(2.0)
-
-W = None
-H = None
 
 ct = CentroidTracker(maxDisappeared=40, maxDistance=50)
 trackers = []
@@ -46,103 +41,68 @@ totalPeople = 0
 
 def thread_for_capturing_face():
     print("[INFO] Running Thread 1...")
-    global net
-    global total_faces_detected_locally
-    global CLASSES
-    global vs
-    global W
-    global H
-    global ct
-    global trackers
-    global trackableObjects
-    global totalFrames
-    global totalDown
-    global totalUp
-    global totalPeople
-
-    fps = FPS().start()
 
     while True:
-        frame = vs.read()
+        ret, frame = vs.read()
 
-        frame = imutils.resize(frame, width=500)
+        frame = cv2.resize(frame, width=500, interpolation=cv2.INTER_AREA)
         rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        if W is None or H is None:
-            (H, W) = frame.shape[:2]
+        (H, W) = frame.shape[:2]
 
         status = "Waiting"
         rects = []
 
-        if totalFrames % 40 == 0:
+        if totalFrames % 30 == 0:
             status = "Detecting"
             trackers = []
 
             blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
             net.setInput(blob)
             detections = net.forward()
-
+ 
             for i in np.arange(0, detections.shape[2]):
-
                 confidence = detections[0, 0, i, 2]
-
                 if confidence > 0.4:
-
                     idx = int(detections[0, 0, i, 1])
-
                     if CLASSES[idx] != "person":
                         continue
-
                     box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
                     (startX, startY, endX, endY) = box.astype("int")
-
                     tracker = dlib.correlation_tracker()
                     rect = dlib.rectangle(startX, startY, endX, endY)
                     tracker.start_track(rgb, rect)
-
                     trackers.append(tracker)
-
         else:
             # loop over the trackers
             for tracker in trackers:
                 status = "Tracking"
-
                 tracker.update(rgb)
                 pos = tracker.get_position()
-
                 startX = int(pos.left())
                 startY = int(pos.top())
                 endX = int(pos.right())
                 endY = int(pos.bottom())
-
                 rects.append((startX, startY, endX, endY))
 
         cv2.line(frame, (0, W // 2), (H, W // 2), (0, 255, 255), 2)
-
         objects = ct.update(rects)
 
         for (objectID, centroid) in objects.items():
-
             to = trackableObjects.get(objectID, None)
-
             if to is None:
                 to = TrackableObject(objectID, centroid)
-
             else:
-
                 y = [c[1] for c in to.centroids]
                 direction = centroid[1] - np.mean(y)
                 to.centroids.append(centroid)
-
                 if not to.counted:
-
                     if direction < 0 and centroid[1] < W // 2:
                         totalUp += 1
                         totalPeople += 1
                         total_faces_detected_locally += 1
                         # print(type(totalPeople))
                         to.counted = True
-
                     elif direction > 0 and centroid[1] > W // 2:
                         totalPeople -= 1
                         totalDown += 1
@@ -152,7 +112,6 @@ def thread_for_capturing_face():
             if totalPeople == 5:
                 print('Sorry you cannot enter.')
             trackableObjects[objectID] = to
-
             text = "ID {}".format(objectID)
             cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
@@ -169,17 +128,11 @@ def thread_for_capturing_face():
             text = "{}: {}".format(k, v)
             cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
-
         # print("{} people entered in today".format(totalPeople))
 
         totalFrames += 1
-        fps.update()
-
-        fps.stop()
-        # print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-        # print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-
-        cv2.destroyAllWindows()
+        cv2.imshow("Frame", frame)
+        cv2.waitKey(1)
 
 
 def thread_for_receiving_face_detected_by_peer():
@@ -275,4 +228,3 @@ if __name__ == "__main__":
 
     # both threads completely executed
     print("Done!")
-
